9c234b2279b4c5784368e0cccdfc8fd3208da77d
Jeeeeee11 <109201161+Jeeeeee11@users.noreply.github.com>
2023-06-09 05:20:51 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import openai
+import requests
+import re
+
+# Set up your OpenAI API credentials
+openai.api_key = 'YOUR_OPENAI_API_KEY'
+
+# Edamam API credentials
+edamam_app_id = 'YOUR_EDAMAM_APP_ID'
+edamam_app_key = 'YOUR_EDAMAM_APP_KEY'
+
+def generate_dishes(ingredients):
+    # Convert portions to a consistent unit, e.g., grams
+    converted_ingredients = convert_portions_to_grams(ingredients)
+
+    prompt = "You have:\n"
+    for ingredient in converted_ingredients:
+        prompt += f"- {ingredient['amount']} {ingredient['unit']} of {ingredient['name']}\n"
+    prompt += "\nGenerate three different dishes using these ingredients:\n1. "
+
+    # Generate recipe suggestions using OpenAI API
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=200,
+        n=3,
+        stop=None,
+        echo=True
+    )
+
+    dishes = []
+
+    # Parse the generated text to extract the recipe suggestions
+    for choice in response.choices:
+        choice_text = choice.text.strip().split('\n')
+        dish = choice_text[0].split('1. ')[1]
+        directions = '\n'.join(choice_text[1:])
+        dishes.append((dish, directions))
+
+    return dishes
+
+def convert_portions_to_grams(ingredients):
+    converted_ingredients = []
+
+    for ingredient in ingredients:
+        amount, unit, name = extract_amount_unit_name(ingredient)
+        amount_in_grams = convert_to_grams(amount, unit)
+        converted_ingredients.append({'amount': amount_in_grams, 'unit': 'grams', 'name': name})
+
+    return converted_ingredients
+
+def extract_amount_unit_name(ingredient):
+    amount = re.findall(r'\d+', ingredient)[0]
+    unit = re.findall(r'\b(?:piece|pieces|cloves)\b', ingredient)
+    if unit:
+        unit = unit[0]
+    else:
+        unit = ""
+    name = re.sub(r'\d+|\b(?:piece|pieces|cloves)\b', '', ingredient).strip()
+    return amount, unit, name
+
+def convert_to_grams(amount, unit):
+    # Implement your own conversion logic here
+    if unit == 'pieces':
+        grams = float(amount) * 100  # Assuming a piece is approximately 100 grams
+    elif unit == 'cloves':
+        grams = float(amount) * 4  # Assuming a clove is approximately 4 grams
+    else:
+        grams = float(amount)
+    return grams
+
+def get_recipe_image(title):
+    # Make a request to Edamam API to retrieve recipe details
+    response = requests.get(f"https://api.edamam.com/api/recipes/v2?type=public&q={title}&app_id={edamam_app_id}&app_key={edamam_app_key}")
+
+    # Parse the response to extract the image URL
+    data = response.json()
+    if 'hits' in data and data['hits']:
+        recipe = data['hits'][0]['recipe']
+        if 'image' in recipe:
+            return recipe['image']
+
+    return None
+
+# Streamlit web app
+def main():
+    st.title("Recipe Generator")
+    st.write("Enter a list of ingredients and generate three different dishes!")
+
+    ingredients_text = st.text_area("Enter the ingredients (separated by commas)")
+    ingredients = [ingredient.strip() for ingredient in ingredients_text.split(",")]
+
+    if st.button("Generate Recipes"):
+        suggested_dishes = generate_dishes(ingredients)
+
+        st.subheader("Inputted Ingredients:")
+        for ingredient in ingredients:
+            st.write("- " + ingredient)
+        
+        st.subheader("Suggested dishes:")
+        for i, dish in enumerate(suggested_dishes):
+            st.write(f"\n{i+1}. {dish[0]}:")
+            st.write(dish[1])
+            image_url = get_recipe_image(dish[0])
+            if image_url:
+                st.image(image_url, use_column_width=True)
+            else:
+                st.write("No image available.")
+
+if __name__ == "__main__":
+    main()
